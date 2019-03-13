398ce26f04163f1a9d43700ca6ecfc37daf55019
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-03-13 01:08:51 +0100

    Added support for multiple motifs

--- scripts/motif2GFF3.py
+++ scripts/motif2GFF3.py
@@ -9,22 +9,26 @@ from Bio.SeqRecord import SeqRecord
 def motif_gff3_forward_strand(args):
   seqioparse=SeqIO.parse(args.input_genome_fasta_filepath, "fasta")
   outentries=""
-  for seq_record in seqioparse:
-    coordinates = [m.span() for m in re.finditer(str(args.motif_string),str(seq_record.seq))]
-    for (start_coordinate,end_coordinate) in coordinates:
-        motifentry = seq_record.id + "\t" + "." + "\t" + "nucleotide_motif" + "\t" + str(start_coordinate + 1) + "\t" +  str(end_coordinate)  + "\t" + "." + "\t" + "+" + "\t" + "." + "\t" + "Note=motif;\n"
-        outentries+=motifentry
+  motifs=args.motif_string(",")
+  for motif in motifs
+    for seq_record in seqioparse:
+      coordinates = [m.span() for m in re.finditer(str(motif),str(seq_record.seq))]
+      for (start_coordinate,end_coordinate) in coordinates:
+          motifentry = seq_record.id + "\t" + "." + "\t" + "nucleotide_motif" + "\t" + str(start_coordinate + 1) + "\t" +  str(end_coordinate)  + "\t" + "." + "\t" + "+" + "\t" + "." + "\t" + "\n"
+          outentries+=motifentry
   return(outentries)
 
 def motif_gff3_reverse_strand(args):
   seqioparse=SeqIO.parse(args.input_reverse_genome_fasta_filepath, "fasta")
   outentries=""
-  for seq_record in seqioparse:
-    reverse_sequence=str(seq_record.seq)
-    length_reverse_seq=len(reverse_sequence)
-    coordinates = [m.span() for m in re.finditer(str(args.motif_string),reverse_sequence)]
-    for (start_coordinate,end_coordinate) in coordinates:
-        motifentry = seq_record.id + "\t" + "." + "\t" + "nucleotide_motif" + "\t" + str(length_reverse_seq - end_coordinate + 1) + "\t" +  str(length_reverse_seq - start_coordinate)  + "\t" + "." + "\t" + "-" + "\t" + "." + "\t" + "Note=motif;\n"
+  motifs=args.motif_string(",")
+  for motif in motifs
+    for seq_record in seqioparse:
+      reverse_sequence=str(seq_record.seq)
+      length_reverse_seq=len(reverse_sequence)
+      coordinates = [m.span() for m in re.finditer(str(motif_string),reverse_sequence)]
+      for (start_coordinate,end_coordinate) in coordinates:
+        motifentry = seq_record.id + "\t" + "." + "\t" + "nucleotide_motif" + "\t" + str(length_reverse_seq - end_coordinate + 1) + "\t" +  str(length_reverse_seq - start_coordinate)  + "\t" + "." + "\t" + "-" + "\t" + "." + "\t" + "\n"
         outentries+=motifentry
   return(outentries)
 
@@ -32,7 +36,7 @@ def motif_gff3_reverse_strand(args):
 def main():
     # store commandline args
     parser = argparse.ArgumentParser(description='Searches the genome for motifs and builds gff3 tracks for hits.')
-    parser.add_argument("--motif_string", help='Motif string')
+    parser.add_argument("--motif_string", help='Comma separated motif strings')
     parser.add_argument("--output_gff3_filepath", help='Path to write gff3 output')
     parser.add_argument("--input_genome_fasta_filepath", help='Path to read genome fasta input')
     parser.add_argument("--input_reverse_genome_fasta_filepath", help='Path to read reverse genome fasta input')
