11b374b1219c825e340a81e1504913871d319d36
RickGelhausen <rick.gelhausen@gmail.com>
2019-04-01 13:19:08 +0200

    fixed processAnnotation script

--- scripts/processAnnotation.py
+++ scripts/processAnnotation.py
@@ -3,6 +3,18 @@ import csv
 import pandas as pd
 import argparse
 import re
+import collections
+import fileinput
+
+# helper function used to efficiently prepend lines to a file
+# https://stackoverflow.com/questions/5914627/prepend-line-to-beginning-of-a-file
+def line_pre_adder(filename, line):
+    f = fileinput.input(filename, inplace=1)
+    for xline in f:
+        if f.isfirstline():
+            print(line.rstrip('\r\n') + '\n' + xline)
+        else:
+            print(xline)
 
 # little helper function to create named tuple without having to always state every argument
 def createNTuple(row, s0=None, s1=None, s2=None, s3=None, s4=None, s5=None, s6=None, s7=None, s8=None):
@@ -35,7 +47,7 @@ def createNTuple(row, s0=None, s1=None, s2=None, s3=None, s4=None, s5=None, s6=N
         c8 = s8
     return nTuple(c0, c1, c2, c3, c4, c5, c6, c7, c8)
 
-
+# Reformat the attribute column exactly how metagene wants it
 def combineSplit(attrSplit):
     attribute = ""
     attrCount = 0
@@ -64,23 +76,43 @@ def combineSplit(attrSplit):
     return attribute
 
 def process_annotation(args):
+    allowedFeatures= ["exon", "cds", "start_codon", "stop_codon", "gene", "transcript"]
     # read input annotation
     annDF = pd.read_csv(args.annotation, sep="\t", comment="#", header=None)
 
     # check if gff2/gtf or gff3 format
-    if annDF[8].str.contains("ID="):
+
+    # number of entries containing ID=
+    trueValues = sum(annDF[8].str.contains("ID=").tolist())
+    # number of rows
+    nrows = len(annDF.index)
+    # if 75% of all rows contain ID= it is likely a gff3
+    if trueValues >= nrows * 0.75:
         # gff3
         annDF.to_csv(args.output, sep="\t", header=False, index=False, quoting=csv.QUOTE_NONE)
+        line_pre_adder(args.output, "##gff-version 3")
     else:
         # gff2
-        # only accept rows that contain gene_id transcript_id combination
-        annDF = annDF[annDF[8].str.contains("gene_id") & annDF[8].str.contains("transcript_id")]
+        # only accept rows that contain gene_id or transcript_id, then generate the missing
+        annDF = annDF[annDF[8].str.contains("gene_id") | annDF[8].str.contains("transcript_id")]
 
         # ensure that the separation of attributes is done correctly
         rows = []
         for row in annDF.itertuples(index=False, name='Pandas'):
+            # ignore entries that do not contain wanted features
+            if getattr(row, "_2").lower() not in allowedFeatures:
+                continue
+
             attributes = getattr(row, "_8")
             attrSplit = list(filter(None, re.split('[ ;]', attributes)))
+
+            # if either gene_id or transcript_id is missing, add an empty one
+            if "gene_id" in attributes:
+                if not "transcript_id" in attributes:
+                    attrSplit = attrSplit[:2] + ['transcript_id','""'] + attrSplit[2:]
+            elif "transcript_id" in attributes:
+                if not "gene_id" in attributes:
+                    attrSplit = ['gene_id','""'] + attrSplit
 
             attributes = combineSplit(attrSplit)
             rows.append(createNTuple(row, s8=attributes))
@@ -89,7 +121,7 @@ def process_annotation(args):
 
         # write processed annotation to output
         annDF.to_csv(args.output, sep="\t", header=False, index=False, quoting=csv.QUOTE_NONE)
-
+        line_pre_adder(args.output, "##gff-version 2")
 
 def main():
     # store commandline args
