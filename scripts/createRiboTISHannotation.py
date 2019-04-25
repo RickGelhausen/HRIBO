a50c429be41889c613ab226b4d492c72b07c03aa
RickGelhausen <rick.gelhausen@gmail.com>
2019-04-25 10:34:49 +0200

    correcting negative boundaries for ribotish annotation

--- scripts/createRiboTISHannotation.py
+++ scripts/createRiboTISHannotation.py
@@ -11,7 +11,7 @@ import collections
 def create_dictionary(input_df):
     print("Reading table rows!")
 
-    gene_dict = dict()
+    gene_dict = {}
     for row in input_df.itertuples(index=False, name='Pandas'):
         attributes = getattr(row, "_8")
 
@@ -39,14 +39,34 @@ def change_annotation(args):
         sys.exit("Error reading the annotation file, please ensure that it is formatted correctly.\n"\
                 +"Exiting with:\n   %s" % error)
 
+    # read the genome size file into dictionary
+    sizes_dict = {}
+    genome_sizes = [line.rstrip("\n") for line in open(args.genome_sizes)]
+    for genome in genome_sizes:
+        key, val = genome.split("\t")
+        if key not in sizes_dict:
+            sizes_dict[key] = int(val)
+
+
     nTuple = collections.namedtuple('Pandas', ["s0","s1","s2","s3","s4","s5","s6","s7","s8"])
 
     wanted_features = ["gene", "exon", "CDS"]
     gene_dict = create_dictionary(annotation_df)
+    # use the dictionary to create a dataframe containg all the required information
     rows = []
     for key, val in gene_dict.items():
+        region = val[0]
+        start = int(val[1])
+        stop = int(val[2])
+
+        # correct the start and stop for ribotish
+        if start < 0 or stop < 0:
+            start += sizes_dict[region]
+            stop += sizes_dict[region]
+
+        # create the wanted features
         for feature in wanted_features:
-            rows.append(nTuple(val[0], "generated", feature, val[1], val[2], val[3], val[4], val[5], val[6]))
+            rows.append(nTuple(region, "generated", feature, start, stop, val[3], val[4], val[5], val[6]))
 
     return pd.DataFrame.from_records(rows, columns=[0,1,2,3,4,5,6,7,8])
 
@@ -57,7 +77,8 @@ def main():
                         , help="the annotation file to be validated.")
     parser.add_argument("--annotation_output", action="store", dest="annotation_output", required=True
                         , help="output annotation file.")
-
+    parser.add_argument("--genome_sizes", action="store", dest="genome_sizes", required=True
+                        , help="file containing genome size file.")
     args = parser.parse_args()
 
     df = change_annotation(args)
