445a8d48325a501a7072c5ca439fc52e0bfbfc01
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2021-06-09 14:44:15 +0000

    Added readlengthstat to lib

--- scripts/lib/library.py
+++ scripts/lib/library.py
@@ -11,7 +11,7 @@ import json
 import statistics
 import math
 import os
-
+import ntpath
 
 def get_start_codons(input_gff_filepath):
     """
@@ -390,3 +390,59 @@ def metagene_mapping(length, length_reads, seqid, codons, strand, input_type, no
                 globalmapping[abs(coveragelower):abs(coverageupper)] += 1
 
     return length, globalmapping, fiveprimemapping, centeredmapping, threeprimemapping
+
+
+def readlengthstats(input_bam_filepath,min_read_length,max_read_length,out_folder_filepath):
+    readlengths = []
+    readcounts = []
+    readlength = min_read_length
+    length_count_dict={}
+    count_length_dict={}
+    while readlength <= max_read_length:
+        bamfile = pysam.AlignmentFile(input_bam_filepath, "rb")
+        readcounter = 0
+        for read in bamfile.fetch():
+            if not (read.is_unmapped):
+                if not (read.is_reverse):
+                    currentreadlength = read.query_alignment_length
+                    if currentreadlength == readlength:
+                        readcounter += 1
+        length_count_dict[readlength]=readcounter
+        count_length_dict[readcounter]=readlength
+        readlengths.append(readlength)
+        readcounts.append(readcounter)
+        readlength += 1
+    #print(length_count_dict)
+    #print(count_length_dict)
+    peaks, properties=find_peaks(readcounts,width=[1,7])
+    #print(peaks)
+    readlength_peaks=[readlengths[x] for x in peaks]
+    #find max peak
+    ymaxs=[length_count_dict.get(key) for key in readlength_peaks]
+    index_max_peak=ymaxs.index(max(ymaxs))
+    #print(readlength_peaks)
+    plt.plot(readlengths, readcounts)
+    xmins=[readlengths[int(math.floor(left))] for left in properties["left_ips"]]
+    #print(xmins)
+    xmaxs=[readlengths[int(math.ceil(right))] for right in properties["right_ips"]]
+    #print(xmaxs)
+    plt.hlines(y=properties["width_heights"], xmin = xmins, xmax = xmaxs)
+    ymaxs=[length_count_dict.get(key) for key in readlength_peaks]
+    #print(ymaxs)
+    ymins =[0 for key in readlength_peaks]
+    #print(ymins)
+    max_length=readlength_peaks[index_max_peak]
+    max_lower=xmins[index_max_peak]
+    max_upper=xmaxs[index_max_peak]
+    json_file_path=out_folder_filepath + ntpath.basename(input_bam_filepath) +  "_read_length_distribution.json"
+    json_file = open(json_file_path, 'w')
+    json.dump(length_count_dict, json_file)
+    txt_file_path = out_folder_filepath + ntpath.basename(input_bam_filepath) +  "_read_length_distribution.txt"
+    txt_file = open(txt_file_path, 'w')
+    txt_file.write(str(max_length) + ":" + str(max_lower) + "-" + str(max_upper))
+    txt_file.close()
+    plt.vlines(x=readlength_peaks, ymin=ymins, ymax = ymaxs)
+    plt.xlabel('read_lengths')
+    plt.ylabel('counts')
+    plt.savefig(out_folder_filepath + ntpath.basename(input_bam_filepath) +  "_read_length_distribution.pdf", format='pdf')
+
--- scripts/readlengthstat.py
+++ scripts/readlengthstat.py
@@ -8,60 +8,7 @@ from scipy.signal import find_peaks
 import ntpath
 import json
 import math
-
-def readlengthstats(input_bam_filepath,min_read_length,max_read_length,out_folder_filepath):
-    readlengths = []
-    readcounts = []
-    readlength = min_read_length
-    length_count_dict={}
-    count_length_dict={}
-    while readlength <= max_read_length:
-        bamfile = pysam.AlignmentFile(input_bam_filepath, "rb")
-        readcounter = 0
-        for read in bamfile.fetch():
-            if not (read.is_unmapped):
-                if not (read.is_reverse):
-                    currentreadlength = read.query_alignment_length
-                    if currentreadlength == readlength:
-                        readcounter += 1
-        length_count_dict[readlength]=readcounter
-        count_length_dict[readcounter]=readlength
-        readlengths.append(readlength)
-        readcounts.append(readcounter)
-        readlength += 1
-    #print(length_count_dict)
-    #print(count_length_dict)
-    peaks, properties=find_peaks(readcounts,width=[1,7])
-    #print(peaks)
-    readlength_peaks=[readlengths[x] for x in peaks]
-    #find max peak
-    ymaxs=[length_count_dict.get(key) for key in readlength_peaks]
-    index_max_peak=ymaxs.index(max(ymaxs))
-    #print(readlength_peaks)
-    plt.plot(readlengths, readcounts)
-    xmins=[readlengths[int(math.floor(left))] for left in properties["left_ips"]]
-    #print(xmins)
-    xmaxs=[readlengths[int(math.ceil(right))] for right in properties["right_ips"]]
-    #print(xmaxs)
-    plt.hlines(y=properties["width_heights"], xmin = xmins, xmax = xmaxs)
-    ymaxs=[length_count_dict.get(key) for key in readlength_peaks]
-    #print(ymaxs)
-    ymins =[0 for key in readlength_peaks]
-    #print(ymins)
-    max_length=readlength_peaks[index_max_peak]
-    max_lower=xmins[index_max_peak]
-    max_upper=xmaxs[index_max_peak]
-    json_file_path=out_folder_filepath + ntpath.basename(input_bam_filepath) +  "_read_length_distribution.json"
-    json_file = open(json_file_path, 'w')
-    json.dump(length_count_dict, json_file)
-    txt_file_path = out_folder_filepath + ntpath.basename(input_bam_filepath) +  "_read_length_distribution.txt"
-    txt_file = open(txt_file_path, 'w')
-    txt_file.write(str(max_length) + ":" + str(max_lower) + "-" + str(max_upper))
-    txt_file.close()
-    plt.vlines(x=readlength_peaks, ymin=ymins, ymax = ymaxs)
-    plt.xlabel('read_lengths')
-    plt.ylabel('counts')
-    plt.savefig(out_folder_filepath + ntpath.basename(input_bam_filepath) +  "_read_length_distribution.pdf", format='pdf')
+import lib.library as hribo
 
 
 def main():
@@ -72,7 +19,7 @@ def main():
     parser.add_argument("--max_read_length", help='Maximal read length to consider', type=int, default=50)
     parser.add_argument("--out_folder_filepath", help='Directory path to write output files, if not present the directory will be created', required=True)
     args = parser.parse_args()
-    readlengthstats(args.in_bam_filepath,args.min_read_length,args.max_read_length,args.out_folder_filepath)
+    hribo.readlengthstats(args.in_bam_filepath,args.min_read_length,args.max_read_length,args.out_folder_filepath)
 
 
 if __name__ == '__main__':
