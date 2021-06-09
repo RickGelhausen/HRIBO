faeb00cf0d55b958e59f8adbb2415e90ab63c288
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2021-06-09 14:33:01 +0000

    Linting

--- scripts/lib/library.py
+++ scripts/lib/library.py
@@ -1,19 +1,17 @@
 import numpy as np
-import regex as re
 import pysam
 import csv
 import matplotlib.pyplot as plt
 import pandas as pd
 import interlap
-import random
 from scipy.signal import find_peaks
 from collections import defaultdict
 from multiprocessing import Pool
 import json
-import numbers
 import statistics
 import math
 import os
+
 
 def get_start_codons(input_gff_filepath):
     """
@@ -24,20 +22,20 @@ def get_start_codons(input_gff_filepath):
     with open(input_gff_filepath, newline='\n') as csvfile:
         gffreader = csv.reader(csvfile, delimiter='\t')
         for entry in gffreader:
-            #filter empty and comment lines
+            # filter empty and comment lines
             if (not len(entry) == 0 and not entry[0].startswith('#')):
                 gfftype = entry[2]
                 if gfftype == "CDS":
                     seqid = entry[0]
                     seqid_set.add(seqid)
-                    source = entry[1]
+                    # source = entry[1]
                     gfftype = entry[2]
                     start = int(entry[3]) - 1
                     end = int(entry[4]) - 1
-                    score = entry[5]
+                    # score = entry[5]
                     strand = entry[6]
-                    phase = entry[7]
-                    attributes = entry[8]
+                    # phase = entry[7]
+                    # attributes = entry[8]
                     if strand == "+":
                         startcodons[strand].append(Feature("start", seqid, start, str(start+2), strand))
                     else:
@@ -57,20 +55,21 @@ def get_stop_codons(input_gff_filepath):
                 if gfftype == "CDS":
                     seqid = entry[0]
                     seqid_set.add(seqid)
-                    source = entry[1]
+                    # source = entry[1]
                     gfftype = entry[2]
                     start = int(entry[3]) - 1
                     end = int(entry[4]) - 1
-                    score = entry[5]
+                    # score = entry[5]
                     strand = entry[6]
-                    phase = entry[7]
-                    attributes = entry[8]
+                    # phase = entry[7]
+                    # attributes = entry[8]
                     if strand == "+":
                         stopcodons[strand].append(Feature("stop", seqid, str(end-2), end, strand))
                     else:
                         stopcodons[strand].append(Feature("stop", seqid, start, str(start+2), strand))
     seqids = list(seqid_set)
     return seqids, stopcodons
+
 
 class Feature:
     def __init__(self, gfftype, seqid, start, end, strand):
@@ -83,11 +82,13 @@ class Feature:
     def __str__(self):
         return self.gfftype + "_" + self.seqid + "_" + self.start + "_" + self.end + "_" + self.strand
 
+
 def get_overlap_bounderies(a, b):
     """
     get the overlap between two intervals
     """
     return (max(a[0], b[0]), min(a[1], b[1]))
+
 
 def get_genome_sizes_dict(input_fai_filepath):
     """
@@ -100,9 +101,10 @@ def get_genome_sizes_dict(input_fai_filepath):
             genome_sizes_dict[entry[0]] = int(entry[1])
     return(genome_sizes_dict)
 
-#Metagene profiling
-#For individual read lengths and indidividual strands and as summarized
-#global, 5', 3', centered profiling for reads overlapping with area around the start codon
+# Metagene profiling
+# For individual read lengths and indidividual strands and as summarized
+# global, 5', 3', centered profiling for reads overlapping with area around the start codon
+
 
 def meta_geneprofiling_p(input_type, in_gff_filepath, in_bam_filepath, out_plot_filepath, cpu_cores, min_read_length, max_read_length, normalization, noise_reduction_analysis, in_readlengthstat_filepath):
     """
@@ -112,12 +114,12 @@ def meta_geneprofiling_p(input_type, in_gff_filepath, in_bam_filepath, out_plot_
     if input_type == "TIS":
         seqids, codons = get_start_codons(in_gff_filepath)
     else:
-        seqids, codons =  get_stop_codons(in_gff_filepath)
+        seqids, codons = get_stop_codons(in_gff_filepath)
     if in_readlengthstat_filepath:
         if os.path.isfile(in_readlengthstat_filepath) and os.access(in_readlengthstat_filepath, os.R_OK):
             with open(in_readlengthstat_filepath) as json_file:
                 length_reads_dict = json.load(json_file)
-                max_reads_length = max(length_reads_dict,key=length_reads_dict.get)
+                max_reads_length = max(length_reads_dict, key=length_reads_dict.get)
                 min_read_length = int(max_reads_length)-2
                 max_read_length = int(max_reads_length)+2
     forward_length_reads_dict = defaultdict(list)
@@ -151,25 +153,24 @@ def meta_gene_profiling(input_type, seqids, cpu_cores, forward_length_reads_dict
         threeprimeforwardprofiles = dfObj = pd.DataFrame()
         centeredforwardprofiles = dfObj = pd.DataFrame()
         if normalization:
-                summarylabel = "mean"
+            summarylabel = "mean"
         else:
-                summarylabel = "sum"
-
+            summarylabel = "sum"
         for (length, globalforwardmapping, fiveprimeforwardmapping, centeredforwardmapping, threeprimeforwardmapping) in threadsforward:
-            globalforwardprofiles[length]=pd.Series(globalforwardmapping)
-            fiveprimeforwardprofiles[length]=pd.Series(fiveprimeforwardmapping)
-            centeredforwardprofiles[length]=pd.Series(centeredforwardmapping)
-            threeprimeforwardprofiles[length]=pd.Series(threeprimeforwardmapping)
+            globalforwardprofiles[length] = pd.Series(globalforwardmapping)
+            fiveprimeforwardprofiles[length] = pd.Series(fiveprimeforwardmapping)
+            centeredforwardprofiles[length] = pd.Series(centeredforwardmapping)
+            threeprimeforwardprofiles[length] = pd.Series(threeprimeforwardmapping)
         threadsreverse = pool.starmap(metagene_mapping, [(length, list(reverse_length_reads_dict.get(length)), seqid, codons, "-", input_type, noise_reduction_analysis) for length in sorted(reverse_length_reads_dict)])
         globalreverseprofiles = dfObj = pd.DataFrame()
         fiveprimereverseprofiles = dfObj = pd.DataFrame()
         threeprimereverseprofiles = dfObj = pd.DataFrame()
         centeredreverseprofiles = dfObj = pd.DataFrame()
         for (length, globalreversemapping, fiveprimereversemapping, centeredreversemapping, threeprimereversemapping) in threadsreverse:
-            globalreverseprofiles[length]=pd.Series(globalreversemapping)
-            fiveprimereverseprofiles[length]=pd.Series(fiveprimereversemapping)
-            centeredreverseprofiles[length]=pd.Series(centeredreversemapping)
-            threeprimereverseprofiles[length]=pd.Series(threeprimereversemapping)
+            globalreverseprofiles[length] = pd.Series(globalreversemapping)
+            fiveprimereverseprofiles[length] = pd.Series(fiveprimereversemapping)
+            centeredreverseprofiles[length] = pd.Series(centeredreversemapping)
+            threeprimereverseprofiles[length] = pd.Series(threeprimereversemapping)
         globalforwardprofiles.loc[:,summarylabel] = globalforwardprofiles.sum(axis=1)
         fiveprimeforwardprofiles.loc[:,summarylabel] = fiveprimeforwardprofiles.sum(axis=1)
         centeredforwardprofiles.loc[:,summarylabel] = centeredforwardprofiles.sum(axis=1)
@@ -196,6 +197,7 @@ def meta_gene_profiling(input_type, seqids, cpu_cores, forward_length_reads_dict
         plotprofile(centeredprofiles, seqid, out_plot_filepath, "centered", normalization, input_type, noise_reduction_analysis)
         plotprofile(threeprimeprofiles, seqid, out_plot_filepath, "threeprime", normalization, input_type, noise_reduction_analysis)
 
+
 def split_evenly(column_names, num_chunks):
     """
     split a list of column names in approximately equal sized chunks
@@ -209,6 +211,7 @@ def assign_color_list(data_split, color_list):
     for ds in range(len(data_split)):
         custom_colors.append([color_list[x] for x in range(ds, len(color_list), len(data_split))])
     return custom_colors
+
 
 def find_optimal_offset(peaks,col):
     window_dict={}
@@ -232,12 +235,13 @@ def find_optimal_offset(peaks,col):
     else:
         return("No peaks","no offset")
 
+
 def plotprofile(profiles, seqid, out_plot_filepath, profiletype, normalization, input_type, noise_reduction_analysis):
     """
     Generate plot for the metagene profiling
     """
-    (window_length,before_start_plus,after_start_plus,before_start_minus,after_start_minus) = set_window(input_type,noise_reduction_analysis)
-    if normalization == True:
+    (window_length, before_start_plus, after_start_plus, before_start_minus, after_start_minus) = set_window(input_type, noise_reduction_analysis)
+    if normalization is True:
         columns = list(profiles)
         for i in columns:
             total = profiles[i].sum()
@@ -252,43 +256,40 @@ def plotprofile(profiles, seqid, out_plot_filepath, profiletype, normalization, 
     cm = plt.get_cmap('gist_rainbow')
     color_list = [cm(1.*i/len(column_names)) for i in range(len(column_names))]
     max_Y = max(list(profiles.max(numeric_only=True))[:-1])
-    offset_dict={}
+    offset_dict = {}
     if len(column_names) < 8:
-        #print(out_plot_filepath + "/" + seqid + "_" + profiletype)
+        # print(out_plot_filepath + "/" + seqid + "_" + profiletype)
         cur_ax = profiles.plot(x="coordinates", ylim=[0, max_Y + (max_Y * 5) / 100], color=color_list)
         cur_ax.set(xlabel="Position", ylabel="Coverage")
         cur_ax.axvline(x=0, color="grey")
         read_columns = column_names#[1:-1]# profiles.columns[1:-1]
         color_index=0
-        #print(read_columns)
+        # print(read_columns)
         average_read_length = math.floor(statistics.mean([x for x in read_columns if isinstance(x, (int,float))]))
-        #average_read_length = math.floor(statistics.mean(filter(isInstance()columns if isinstance(x, int)]))
+        # average_read_length = math.floor(statistics.mean(filter(isInstance()columns if isinstance(x, int)]))
         for colname in read_columns:
-            col=profiles[colname]
-            if (colname=="sum" or colname=="mean"):
-                readlength= average_read_length
+            col =  profiles[colname]
+            if (colname == "sum" or colname == "mean"):
+                readlength = average_read_length
             else:
-                readlength=int(colname)
-            #peaks, properties=find_peaks(col.values, distance=readlength, width=[readlength-3,readlength+3])#,prominence=(0.1, 1))
-            peaks, properties=find_peaks(col.values, distance=readlength, width=[1,40])#,prominence=(0.1, 1))
-            peaks_offset=[math.floor(x) - 100 for x in peaks]
-            xmins=[x - 100 for x in properties["left_ips"]]
-            xmaxs=[x - 100 for x in  properties["right_ips"]]
-            (optimal_offset,optimal_count)=find_optimal_offset(peaks,col)
-            plt.hlines(y=properties["width_heights"], xmin = xmins, xmax = xmaxs, color = color_list[color_index])
-            plt.vlines(x=peaks_offset, ymin=col[peaks] - properties["prominences"], ymax = col[peaks], color = color_list[color_index])
-            color_index=color_index + 1
-            offset_dict[colname]=str(optimal_offset) + "," +  str(optimal_count)
-        json_file_path = out_plot_filepath + "/" + seqid + "_" + profiletype +  "_length_offset.json"
+                readlength = int(colname)
+            # peaks, properties=find_peaks(col.values, distance=readlength, width=[readlength-3,readlength+3])#,prominence=(0.1, 1))
+            peaks, properties = find_peaks(col.values, distance=readlength, width=[1,40])#,prominence=(0.1, 1))
+            peaks_offset =[math.floor(x) - 100 for x in peaks]
+            xmins = [x - 100 for x in properties["left_ips"]]
+            xmaxs = [x - 100 for x in  properties["right_ips"]]
+            (optimal_offset,optimal_count) = find_optimal_offset(peaks,col)
+            plt.hlines(y=properties["width_heights"], xmin=xmins, xmax=xmaxs, color=color_list[color_index])
+            plt.vlines(x=peaks_offset, ymin=col[peaks] - properties["prominences"], ymax=col[peaks], color=color_list[color_index])
+            color_index = color_index + 1
+            offset_dict[colname] = str(optimal_offset) + "," + str(optimal_count)
+        json_file_path = out_plot_filepath + "/" + seqid + "_" + profiletype + "_length_offset.json"
         json_file = open(json_file_path, 'w')
         json.dump(offset_dict, json_file)
         plt.savefig(out_plot_filepath + "/" + seqid + "_" + profiletype + "_profiling.pdf", format='pdf')
         plt.close()
-
-
     elif len(column_names) >= 8 and len(column_names) < 16:
         data_split = split_evenly(column_names, 2)
-
         custom_colors = assign_color_list(data_split, color_list)
         fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(12, 5))
         custom_axes = [ax1, ax2]
@@ -296,7 +297,6 @@ def plotprofile(profiles, seqid, out_plot_filepath, profiletype, normalization, 
             cur_ax = profiles.plot(ax=custom_axes[i], x="coordinates", y=data_split[i], ylim=[0, max_Y + (max_Y * 5) / 100], color=custom_colors[i])
             cur_ax.set(xlabel="Position", ylabel="Coverage")
             cur_ax.axvline(x=0, color="grey")
-
         plt.savefig(out_plot_filepath + "/" + seqid + "_" + profiletype + "_profiling.pdf", format='pdf')
         plt.close()
 
@@ -304,7 +304,6 @@ def plotprofile(profiles, seqid, out_plot_filepath, profiletype, normalization, 
         data_split = split_evenly(column_names, 4)
         custom_colors = assign_color_list(data_split, color_list)
         fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(12, 10))
-
         custom_axes = [axes[0,0], axes[0,1], axes[1,0], axes[1,1]]
         for i in range(4):
             cur_ax = profiles.plot(ax=custom_axes[i], x="coordinates", y=data_split[i], ylim=[0, max_Y + (max_Y * 5) / 100], color=custom_colors[i])
